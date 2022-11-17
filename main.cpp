af4b421524909cafd18d38c6acdb7bd254ced207
León G <A00573074@itesm.mx>
2022-11-17 16:29:13 +0000

    New

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ void calcularSubArreglo(long long int datos[], long long int x[], int n, int c) 
 //Función que implementa el algoritmo de Meet In The Middle, recibe un arreglo de long long ints, un entero objetivo, y un long long int s. Retorna el valor resultante long long int.
 long long int reunionEnElMedio(long long int datos[], int n, long long int objetivo) { //Complejidad Computacional: O(2^(n/2) log(2^(n/2))).
 
-	calcularSubArreglo(datos, valoresX, n/2, 0); //Complejidad Computacional: O( (n/2) 2^ (n/2)).
+	
 	calcularSubArreglo(datos, valoresY, n-n/2, n/2); //Complejidad Computacional: O((n-n/2) 2^ (n-n/2)).
 
 	int tamanioX;
